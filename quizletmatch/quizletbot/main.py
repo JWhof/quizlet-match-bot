72b1148b7cdeaf498ac2aa401feeb829360fe281
Jurgen Westerhof <romanczyy@gmail.com>
2023-04-11 12:59:03 +0200

    change main.py, major mods
    -overhaul of main.py
    -changed coordinate finding system, much faster
    -locates small image of top left corner of match cards to find coords
    -TODO: add text detection at given coods & matching logic

--- quizletmatch/quizletbot/main.py
+++ quizletmatch/quizletbot/main.py
@@ -1,97 +1,24 @@
-import codecs
+import pyautogui as pag
 import time
 
-import pyautogui
-import pyocr
-from PIL import Image
-
-lang = 'eng'
-
-tool = pyocr.get_available_tools()[0]
-
-line_box_builder = pyocr.builders.LineBoxBuilder()
-text_builder = pyocr.builders.TextBuilder()
-line_box_dict = {}
-
-def capture_screenshot(filename):
-    time.sleep(3)  # wait for 3 seconds before capturing the screenshot
-    pyautogui.press('enter')
-    time.sleep(0.3)
-    screenshot = pyautogui.screenshot(region=(400,310,4070,2210))
-
-    print(f"pyauto width, height: {screenshot.size}")
-    screenshot.save(filename)
-    return Image.open(filename)
-
-def scale_line_box_position(line_box, scale_factor):
-    x1, y1 = line_box.position[0]
-    x2, y2 = line_box.position[1]
-
-    scaled_x1 = int(x1 * scale_factor)
-    scaled_y1 = int(y1 * scale_factor)
-    scaled_x2 = int(x2 * scale_factor)
-    scaled_y2 = int(y2 * scale_factor)
-
-    return ((scaled_x1, scaled_y1), (scaled_x2, scaled_y2))
+NUM_SECONDS_TO_WAIT = 3 # set number of seconds to wait to enter quizlet match window
 
 
-def line_box_to_pyautogui_position(line_box):
-    x1, y1 = line_box.position[0]
-    x2, y2 = line_box.position[1]
+def card_coordinates(): # maybe add number of terms checker?
+    return pag.locateAll('quizletmatch/quizletbot/top_card3.png', pag.screenshot()) # coordinates are shifted up 10x10 due to image taken
 
-    center_x = (x1 + x2) // 2
-    center_y = (y1 + y2) // 2
-
-    return center_x, center_y
-
-
-def perform_matching(line_box_coords):
-    print(f"line box coords: {line_box_coords}")
-    pyautogui.moveTo(line_box_coords[0], line_box_coords[1])
-    pyautogui.click(line_box_coords[0], line_box_coords[1])
-    return
+def write_output(output_file_path, text_to_write):
+    file = open(output_file_path, 'w')
+    file.writelines(text_to_write)
 
 def main():
-    process_duration = 5 
+    time.sleep(NUM_SECONDS_TO_WAIT) # wait desired number of seconds 
+    print("starting.........")
     start_time = time.time()
+    pag.press('enter') # auto press enter
 
-    screenshot_filename = 'quizletbot/screenshot.png'
-    image = capture_screenshot(screenshot_filename)
-
-    print(f"pyorc width, height: {image.size}")
-
-    line_boxes = tool.image_to_string(
-        image,
-        lang=lang,
-        builder=line_box_builder
-    )
-    
-    txt = tool.image_to_string(
-    image,
-    lang=lang,
-    builder=text_builder
-    )
-
-    line_box_dict = {line_box.content:line_box.position for line_box in line_boxes}
-
-    perform_matching(line_box_to_pyautogui_position(line_boxes[0]))
-
-    # potential delay? idk man
-    time.sleep(1)
-
-    # all of this is mostly for testing
-    line_box_tuples = '\n'.join([f"{line_box.content}, {line_box.position}" for line_box in line_boxes])
-
-    with codecs.open("output.txt", 'w', encoding='utf-8') as file_descriptor:
-        text_builder.write_file(file_descriptor, line_box_tuples)
-
-    print("done")
-    elapsed_time = time.time() - start_time
-    if elapsed_time < process_duration+3:
-        time.sleep(process_duration+3 - elapsed_time)
-
-    print(line_box_dict)
-
+    write_output('quizletmatch\quizletbot\card_coords.txt', [f"{coords}\n" for coords in list(card_coordinates())])
+    print(f"total time: {time.time() - start_time}")
 
 if __name__ == "__main__":
     main()
