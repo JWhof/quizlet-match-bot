91212a1ead4822477aee0a8e64499999eb5f7055
Jurgen Westerhof <romanczyy@gmail.com>
2023-04-12 17:28:20 +0200

    add pytesseract and logic IT FINALLY WORKS
    -added relationship between box coordinates and text FINALLY
    -got pytesseract working

--- quizletmatch/quizletbot/main.py
+++ quizletmatch/quizletbot/main.py
@@ -1,12 +1,12 @@
 import pyautogui as pag
 import time
-import math
-import textwrap
+import pytesseract
+from PIL import Image
 
 NUM_SECONDS_TO_WAIT = 3 # set number of seconds to wait to enter quizlet match window
 MAX_CARD_WIDTH_PX = 250 # number of pixels that the box can be
 CARD_FONT_SIZE_PX = 16 # font size of text in card in px
-
+dict_of_coords_and_text = {}
 
 def start_up(): # wait desired number of seconds and display countdown
     for seconds_left in range(NUM_SECONDS_TO_WAIT)[::-1]:
@@ -16,15 +16,17 @@ def start_up(): # wait desired number of seconds and display countdown
     time.sleep(1)
 
 def card_coordinates(): # maybe add number of terms checker?
-    return pag.locateAll('quizletmatch/quizletbot/top_card3.png', pag.screenshot())# coordinates are shifted up 10x10 due to image taken
+    return pag.locateAll('quizletmatch/quizletbot/top_card3.png', pag.screenshot())# coordinates should be shifted right by 2 and down by 1 
 
-def predict_box_size(text, box_width, font_size):
-    char_width = font_size * 0.538 # played around with value until it seemed right
-    chars_per_line = math.floor(box_width / char_width)
-    lines = textwrap.wrap(text, width=chars_per_line)
-    box_height = math.ceil(len(lines) * (font_size * 1.625))+18 # multiply font_size by line-height add 18 for padding
-    
-    return lines, box_height
+def screenshot_region(left, top, file_name):
+    return pag.screenshot(file_name, region=(left + 2, top + 1, MAX_CARD_WIDTH_PX, 96)) # region=left, top, width, height
+
+def screenshot_and_convert_all_cards():
+    for count, box in enumerate(list(card_coordinates())):
+        file_name = f"box_screenshot{count}.png"
+        screenshot_region(box.left, box.top, file_name)
+        image_of_box_screenshot = Image.open(file_name)
+        dict_of_coords_and_text[box] = pytesseract.image_to_string(image_of_box_screenshot).replace("\n", " ")
 
 def write_output(output_file_path, text_to_write):
     file = open(output_file_path, 'w')
@@ -37,9 +39,13 @@ def main():
 
     pag.press('enter') # auto press enter on 'start game' screen
     time.sleep(0.5)
-    write_output('quizletmatch/quizletbot/card_coords.txt', [f"{coords}\n" for coords in list(card_coordinates())])
 
+    screenshot_and_convert_all_cards()
+
+
+    write_output('quizletmatch/quizletbot/card_coords.txt', [f"{box}, {dict_of_coords_and_text[box]}\n" for box in dict_of_coords_and_text.keys()])
     print(f"total time: {time.time() - start_time}")
 
+
 if __name__ == "__main__":
-    print(predict_box_size("Tamara kon het niet laten om het telefoongesprek te ... met een gele opmerking", MAX_CARD_WIDTH_PX, CARD_FONT_SIZE_PX))
+    main()
