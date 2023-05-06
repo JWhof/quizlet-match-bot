02339819527fb76c963a9b6ad36f43ab3286614e
Jurgen Westerhof <romanczyy@gmail.com>
2023-05-06 14:20:05 +0200

    init overhaul, use html code not image detection

---
+++ quizletmatch/quizletbot/test.py
@@ -0,0 +1,6 @@
+my_list = [100, 200]
+
+card_x, card_y = my_list
+
+print(card_x)
+print(card_y)
--- quizletmatch/quizletbot/test_script.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-
-while 1:
-    print(pyautogui.position())
-
-# [(Box(left=2564, top=456, width=36, height=15), ''), (Box(left=3092, top=578, width=36, height=15), ''), 
-# (Box(left=1589, top=602, width=36, height=15), ''), (Box(left=1122, top=788, width=36, height=15), ''), 
-# (Box(left=2551, top=823, width=36, height=15), ''), 
-# (Box(left=355, top=1040, width=36, height=15), ''), (Box(left=187, top=1049, width=36, height=15), '')]
